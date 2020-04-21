63b8b4d3845cbc70a031d824cda36009a65d9b94
Veronica Barcia <veronicabarcia4@gmail.com>
2020-04-21 18:30:14 -0400

    VERY HARD DONE :)

---
+++ modernJs/veryhard.js
@@ -0,0 +1,41 @@
+// VERY HARD: Write a function that takes in a string and outputs the *first* occurrence of a character that does 
+// not repeat itself in that string. Output the first character in a string that is unique.
+// Input: “the quick brown fox jumps over the calm kitten quietly”  Output: “b”
+// Input: “this hat is the greatest!”  Output: “g”
+// Input: “what a wonderful day it has been!”  Output: “o”
+
+let input= 'what a wonderful day it has been';
+//Make into a array
+input=input.split("");
+//returns array with each letter as it's own index 
+// console.log(input);
+
+
+let i=0;
+
+let char=input[0];
+
+input.shift(0)
+
+
+
+let noRepeat= input=>{
+    //e will be the second letter of the sentence
+    for(let e=0; e<=input.length-1;e++){
+        if (char===input[e]){
+            //this is to remove the other repeats in the sentence, so if its not the same as char it will stay,
+            // but letters the same as char is filtered out
+            input=input.filter(letter=>!(letter===char))
+            //goes to the next letter of the sentnce
+            char=input[i];
+            //removes the first letter of the array, so it won't be recounted in the loop
+            input.shift(0);
+            //starts the loop over 
+            e=0;
+    }
+}
+console.log(char);
+}
+
+
+noRepeat(input);
